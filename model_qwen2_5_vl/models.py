15ba3c0a154a732e933130eb84ae10887ca31d02
Anton Shiryaev <med.phisiker@gmail.com>
2025-02-14 04:26:28 +1000

    init

---
+++ model_qwen2_5_vl/models.py
@@ -0,0 +1,137 @@
+from typing import Any, List
+
+import torch
+from model_interface.model_interface import ModelInterface
+from qwen_vl_utils import process_vision_info
+from transformers import AutoProcessor, Qwen2_5_VLForConditionalGeneration
+
+
+class Qwen2_5_VLModel(ModelInterface):
+    def __init__(
+        self,
+        model_name="Qwen2.5-VL-3B-Instruct",
+        system_prompt="",
+        cache_dir="model_cache",
+        device_map=None
+    ):
+        self.model_name = model_name
+        self.system_prompt = system_prompt
+        self.cache_dir = cache_dir
+
+        self.min_pixels = 256 * 28 * 28
+        self.max_pixels = 1536 * 28 * 28  # 1280 * 28 * 28
+        
+        if device_map is None:
+            device_map="auto"
+
+        # default: Load the model on the available device(s)
+        model_path = f"Qwen/{model_name}"
+        self.model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+            model_path,
+            torch_dtype=torch.bfloat16,
+            device_map=device_map,
+            attn_implementation="flash_attention_2",
+            cache_dir=self.cache_dir,
+        )
+
+        # default processor
+        self.processor = AutoProcessor.from_pretrained(model_path, cache_dir=cache_dir)
+
+    def get_message(self, image, question):
+        message = {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": image,
+                    "min_pixels": self.min_pixels,
+                    "max_pixels": self.max_pixels,
+                },
+                {"type": "text", "text": question},
+            ],
+        }
+
+        return message
+
+    def predict_on_image(self, image, question):
+        messages = [self.get_message(image, question)]
+
+        # Preparation for inference
+        text = self.processor.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+        image_inputs, video_inputs = process_vision_info(messages)
+        inputs = self.processor(
+            text=[text],
+            images=image_inputs,
+            videos=video_inputs,
+            padding=True,
+            return_tensors="pt",
+        )
+        inputs = inputs.to("cuda")
+
+        # Inference: Generation of the output
+        generated_ids = self.model.generate(**inputs, max_new_tokens=512)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :]
+            for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+        output_text = self.processor.batch_decode(
+            generated_ids_trimmed,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False,
+        )
+
+        return output_text[0]
+
+    def predict_on_images(self, images: List[Any], question: str) -> str:
+        """Реализация метода для работы с несколькими изображениями."""
+        # Формируем сообщение с несколькими изображениями
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    *[
+                        {
+                            "type": "image",
+                            "image": img,
+                            "min_pixels": self.min_pixels,
+                            "max_pixels": self.max_pixels,
+                        }
+                        for img in images
+                    ],
+                    {"type": "text", "text": question},
+                ],
+            }
+        ]
+
+        # Подготовка входных данных
+        text = self.processor.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=True
+        )
+        image_inputs, video_inputs = process_vision_info(messages)
+
+        # Обработка входных данных
+        inputs = self.processor(
+            text=[text],
+            images=image_inputs,
+            videos=video_inputs,
+            padding=True,
+            return_tensors="pt",
+        ).to("cuda")
+
+        # Генерация ответа
+        generated_ids = self.model.generate(**inputs, max_new_tokens=512)
+        generated_ids_trimmed = [
+            out_ids[len(in_ids) :]
+            for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+        ]
+
+        # Декодирование результата
+        output_text = self.processor.batch_decode(
+            generated_ids_trimmed,
+            skip_special_tokens=True,
+            clean_up_tokenization_spaces=False,
+        )[0]
+
+        return output_text
---
+++ run_doc_classification.py
@@ -0,0 +1,67 @@
+import os
+import subprocess
+
+from model_interface.model_factory import ModelFactory
+
+
+if __name__ == "__main__":
+
+    cache_directory = "model_cache"
+
+    # Сохраняем модели Qwen2-VL в примонтированную папку
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    cache_directory = os.path.join(script_dir, cache_directory)
+
+    # Имена моделей и семейство моделей
+    model_name_1 = "Qwen2.5-VL-7B-Instruct"
+    model_family = "Qwen2.5-VL"
+
+    # Инфо о том где взять класс для семейства моделей
+    package = "model_qwen2_5_vl"
+    module = "models"
+    model_class = "Qwen2_5_VLModel"
+    model_class_path = f"{package}.{module}:{model_class}"
+
+    # Регистрация модели в фабрике
+    ModelFactory.register_model(model_family, model_class_path)
+
+    # создаем модель
+    model_init_params = {
+        "model_name": model_name_1,
+        "system_prompt": "",
+        "cache_dir": "model_cache",
+    }
+
+    model = ModelFactory.get_model(model_family, model_init_params)
+
+    # отвечаем на вопрос о по нескольким картинкам сразу
+    image_path1 = "example_docs/classification/3.jpg"
+    image_path2 = "example_docs/classification/5.jpg"
+    image_path3 = "example_docs/classification/2.jpg"
+    image_path4 = "example_docs/classification/1.jpg"
+    image_path5 = "example_docs/classification/7.png"
+    image_path6 = "example_docs/classification/6.jpg"
+    
+    images=[image_path1, image_path2, image_path3, image_path4, image_path5, image_path6]
+    
+    question = (f"""Количество поданных страниц документов - {len(images)}.
+                Задача: Определите тип каждого документа на предоставленных изображениях и выведите их в виде последовательности цифр, где каждая цифра соответствует определенному типу документа. Ответ должен содержать только порядок цифр, без дополнительного текста.
+                Типы документов:
+                1 - old_tins: свидетельство о постановке на учет физического лица (документ желтого цвета).
+                2 - new_tins: свидетельство о постановке на учет физического лица
+                3 - interest_free_loan_agreement: договор беспроцентном займа
+                4 - snils: страховое свидетельство обязательного пенсионного страхования (документ зеленого цвета).
+                5 - invoice: счет фактура
+                6 - passport: паспорт России
+                Пример ответа: 2,4,5,1,3
+                Пожалуйста, предоставьте ответ в указанном формате.""")
+    
+    print(question)
+    
+    model_answer = model.predict_on_images(
+        images=images, question=question
+    )
+    
+    subprocess.run(["nvidia-smi"])
+
+    print(model_answer)
